8afb7d570d7cbd1e8670332a52a83c0e52cb9da5
lewos <leo.21@live.com.ar>
2018-04-09 05:44:30 -0300

    added .csv to .txt
    Also new parameter to both functions sep that can be ',' ';' or "," ";"

--- txt_to_csv.py
+++ txt_to_csv.py
@@ -4,15 +4,23 @@ def split_text(text, list_of_sep):
     parts = [text[i:j] for i,j in izip_longest(list_of_sep,list_of_sep[1:])]
     return parts
 
-def text_to_csv(path_text_file,name_list, list_of_sep, path_new_file):
+def text_to_csv(path_text_file,name_list, list_of_sep, path_new_file,sep):
     old = open(path_text_file, 'r') 
     new_file = open(path_new_file, 'w')
     name_list.append("\n")
-    new_file.write((",").join(name_list))
+    new_file.write(sep.join(name_list))
     for line in old:
         list_parts = split_text(line,list_of_sep)
-        new_file.write(write_new_file(list_parts,new_file))
+        new_file.write(write_new_file(list_parts,sep))
     new_file.close()
 
-def write_new_file(list_parts,path_new_file):
-    return ",".join(list_parts)
+def write_new_file(list_parts,sep):
+    return sep.join(list_parts)
+	
+def csv_to_txt(path_csv,path_new_txt,sep):
+    txt_file = open(path_new_txt,'w')
+    csv_file = open(path_csv, 'r')
+    for line in csv_file:
+        print(line.replace(sep,""))
+        txt_file.write(line.replace(sep,""))	
+        
