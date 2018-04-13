9e9ae450c69420dfa37445a0064f1d4994230ea7
Carminatti, Leonardo <leonardo.carminatti@accenture.com>
2018-04-13 11:08:40 -0300

    correcion de error al abrir muchos archivos

--- txt_to_csv.py
+++ txt_to_csv.py
@@ -7,12 +7,24 @@ def split_text(text, list_of_sep):
 def text_to_csv(path_text_file,name_list, list_of_sep, path_new_file,sep):
     old = open(path_text_file, 'r') 
     new_file = open(path_new_file, 'w')
+
+    try:
+        name_list.remove("\n")
+    except :
+        pass
+    
     name_list.append("\n")
     new_file.write(sep.join(name_list))
+    
     for line in old:
         list_parts = split_text(line,list_of_sep)
         new_file.write(write_new_file(list_parts,sep))
     new_file.close()
+    
+    try:
+        name_list.remove("\n")
+    except :
+        pass
 
 def write_new_file(list_parts,sep):
     return sep.join(list_parts)
